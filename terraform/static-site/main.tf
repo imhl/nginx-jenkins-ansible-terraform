b50b1c69c83c971f2f6717f22164e7da388b0e31
imhl <25532076+imhl@users.noreply.github.com>
2023-12-24 11:14:00 +0800

    edits

--- terraform/static-site/main.tf
+++ terraform/static-site/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "site" {
-    ami = "ami-042e8287309f5df03"
-    instance_type = "t2.micro"
-    key_name = "devops"
-    vpc_security_group_ids = [ "sg-f72319ab", "sg-0586fa0aa906bfb79" ]
+    ami = "ami-0c7217cdde317cfec"
+    instance_type = "t2.large"
+    key_name = "vockey"
+    vpc_security_group_ids = ["sg-08b286e486218d6c0"]
     tags = {
         Name = var.name
         group = var.group
--- terraform/static-site/variables.tf
+++ terraform/static-site/variables.tf
@@ -1,15 +1,18 @@
 variable "region" {
-    description = "default free tier region"
+  description = "default free tier region"
+  default     = "us-east-1"
 }
 
 variable "profile" {
-    description = "default aws AMI profile cred"
+  description = "default aws AMI profile cred"
 }
 
 variable "name" {
-    description = "name of the instance"
+  description = "name of the instance"
+
 }
 
 variable "group" {
-    description = "name of the ansible dynamic inventory group."
+  description = "name of the ansible dynamic inventory group."
+
 }
